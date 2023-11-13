febe66fd37e153e17cbc5e1d06858879e03a9a13
Dhrumil Patel <dhrumilcse14@gmail.com>
2023-11-13 17:49:18 +0530

    Kirby 3.9.7 + Added More Content

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 
 require 'kirby/bootstrap.php';
 
+define('KIRBY_HELPER_GO', false);
+
 echo (new Kirby)->render();
--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
-    'debug'  => true
+    'debug'  => true,
+    'd4l' => [
+      'static_site_generator' => [
+        'endpoint' => 'generate-static-site', # set to any string like 'generate-static-site' to use the built-in endpoint (necessary when using the blueprint field)
+        'output_folder' => './static', # you can specify an absolute or relative path
+        'preserve' => [], # preserve individual files / folders in the root level of the output folder (anything starting with "." is always preserved)
+        'skip_media' => false, # set to true to skip copying media files, e.g. when they are already on a CDN; combinable with 'preserve' => ['media']
+        'skip_templates' => [] # ignore pages with given templates (home is always rendered)
+      ],
+    ],
 ];
