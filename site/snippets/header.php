36020bb3a6b37142fe58e5dc5360b29be8564245
Dhrumil Patel <dhrumilcse14@gmail.com>
2023-11-14 18:06:18 +0530

    remove metatags line

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -15,16 +15,14 @@
     <meta property="og:url" content="https://dhrumil.ca/" />
     <meta property="og:title" content="Dhrumil Patel" />
     <meta property="og:description" content="Data, Dogs, and Minimalism." />
-    <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+    <meta property="og:image" content="" />
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://dhrumil.ca/" />
     <meta property="twitter:title" content="Dhrumil Patel" />
     <meta property="twitter:description" content="Data, Dogs, and Minimalism." />
-    <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
-
-    <!-- Meta Tags Generated with https://metatags.io -->
+    <meta property="twitter:image" content="" />
 
     <title>Home</title>
 </head>
