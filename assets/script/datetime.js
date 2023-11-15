53c53140f53c7d6c290676a01951dfcbd62a01e8
Dhrumil Patel <dhrumilcse14@gmail.com>
2023-11-15 21:55:21 +0530

    update datetime

--- assets/script/datetime.js
+++ assets/script/datetime.js
@@ -1,12 +1,13 @@
-    // Get current date and time
-    var now = new Date();
+// Get current date and time in Eastern Time (ET)
+var now = new Date();
+var etOptions = { timeZone: 'America/New_York' };
+var etTime = now.toLocaleString('en-US', { ...etOptions, hour12: false });
 
-    // Extract hours, minutes, and seconds in 24-hour format
-    var hours = now.getHours().toString().padStart(2, '0');
-    var minutes = now.getMinutes().toString().padStart(2, '0');
+// Extract hours and minutes in 24-hour format
+var [hours24, minutes] = etTime.split(',')[1].trim().split(':');
 
-    // Create a string in the format "HH:mm:ss"
-    var time = hours + '.' + minutes
+// Create a string in the format "HH:mm"
+var time = hours24 + ':' + minutes;
 
-    // Insert date and time into HTML
-    document.getElementById("datetime").innerHTML = time;
+// Insert date and time into HTML
+document.getElementById("datetime").innerHTML = time;
