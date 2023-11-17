922df8144366f07756d22b6a663b4f978b5b9f16
Dhrumil Patel <dhrumilcse14@gmail.com>
2023-11-17 23:21:12 +0530

    fix url for favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="shortcut icon" href="<?= css('assets/image/faveicon.ico') ?>">
+    <link rel="shortcut icon" href="<?= url('assets/image/faveicon.ico') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?= css('assets/css/main.css') ?>
 
