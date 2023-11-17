8c2699397f4b724dd56ce056b7812a3efd29f40b
Dhrumil Patel <dhrumilcse14@gmail.com>
2023-11-17 23:17:13 +0530

    update meta title and add favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,19 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" href="<?= css('assets/image/faveicon.ico') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?= css('assets/css/main.css') ?>
 
     <!-- Meta -->
     <!-- Primary Meta Tags -->
     <title><?= $page->isHomePage() ? 'Dhrumil Patel' : $page->title() . ' &mdash; Dhrumil'; ?></title>
-    <meta name="title" content="<?= $page->isHomePage() ? 'Dhrumil Patel' : $page->title() . ' &mdash; Dhrumil'; ?>" />
+    <meta name="title" content="<?= $page->isHomePage() ? 'Dhrumil Patel' : $page->title(); ?>" />
     <meta name="description" content="<?= $page->description() ?>" />
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://dhrumil.ca/" />
-    <meta property="og:title" content="<?= $page->isHomePage() ? 'Dhrumil Patel' : $page->title() . ' &mdash; Dhrumil'; ?>" />
+    <meta property="og:title" content="<?= $page->isHomePage() ? 'Dhrumil Patel' : $page->title(); ?>" />
     <meta property="og:description" content="<?= $page->description() ?>" />
     <?php $ogImage = $page->isHomePage() ? 'og-light.png' : $page->title() . '.png';    ?>
     <meta property="og:image" content="<?= url('assets/image/' . $ogImage) ?>" />
@@ -23,7 +24,7 @@
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://dhrumil.ca/" />
-    <meta property="twitter:title" content="<?= $page->isHomePage() ? 'Dhrumil Patel' : $page->title() . ' &mdash; Dhrumil'; ?>" />
+    <meta property="twitter:title" content="<?= $page->isHomePage() ? 'Dhrumil Patel' : $page->title(); ?>" />
     <meta property="twitter:description" content="<?= $page->description() ?>" />
     <meta property="twitter:image" content="<?= url('assets/image/' . $ogImage) ?>" />
 
