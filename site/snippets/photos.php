77d5e79ba350c253da45e16e4d1629602325a399
Dhrumil Patel <dhrumilcse14@gmail.com>
2023-11-17 21:00:18 +0530

    snippet for photos

---
+++ site/snippets/photos.php
@@ -0,0 +1,15 @@
+<div class="grid-container">
+    <?php
+    // Get the content from the page
+    $content = $page->text()->kt();
+
+    // Use Kirbytags to extract image filenames
+    $contentWithUrls = $content->kirbytext([
+        'figure' => function ($image) {
+            // Use the custom template for the figure tag
+            return snippet('figure', ['image'=>$image, 'url' => $image->file()->url(), 'alt' => $image->alt(), 'caption' => $image->caption()], true);
+        }
+    ]);
+    ?>
+    
+    <?= $contentWithUrls; ?>
