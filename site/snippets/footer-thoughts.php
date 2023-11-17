4a60790cc18f3e3286a26783fd7adf53febb2537
Dhrumil Patel <dhrumilcse14@gmail.com>
2023-11-17 19:36:34 +0530

    remove script

--- site/snippets/footer-thoughts.php
+++ site/snippets/footer-thoughts.php
@@ -11,13 +11,4 @@
 <div>
 </body>
 
-<script>
-    window.onload = function() {
-        if(!window.location.hash) {
-            window.location = window.location + '#loaded';
-            window.location.reload();
-        }
-    }
-</script>
-
 </html>
