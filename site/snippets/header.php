5330dfa2d39f059a3acbc5bf8027c9d4c7bef827
Dhrumil Patel <dhrumilcse14@gmail.com>
2024-03-23 22:40:22 -0400

    css versioning

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="<?= url('assets/image/favicon.ico') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?= css('assets/css/main.css') ?>
+    <?= css('assets/css/main-v1.0.1.css') ?>
 
     <!-- Meta -->
     <?php
