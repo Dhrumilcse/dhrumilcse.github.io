78ad14e56a273e46fa6eb91e33299b6475eab2ae
Dhrumil Patel <dhrumilcse14@gmail.com>
2025-07-26 14:51:51 -0400

    update font to inter + reduce initial scale to 0.95

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="<?= url('assets/image/favicon.ico') ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=0.95">
     <?= css('assets/css/main-v1.0.5.css') ?>
 
     <!-- Meta -->
